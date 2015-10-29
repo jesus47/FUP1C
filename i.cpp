5523758548db51303d5c578e14723c7fd646c653
jesus47 <hbk47@live.com.mx>
2015-10-28 19:17:32 -0600

    Tareas de programacion

---
+++ i.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int n1;
+int i;
+int main ()
+{
+    printf ("Escribe el digito");
+    scanf ("%d", & n1);
+    while (i <= n1)
+    {
+          printf ("%d\n", i);
+          i = i + 1;
+    }
+    system ("PAUSE");
+}
