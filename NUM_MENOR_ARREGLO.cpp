d9f19fc7b6d0d37def889b439ee1fae1221ace46
1973joel <jchaconvazquez@gmail.com>
2015-11-25 19:42:58 -0600

    Numero Menor Arreglos

--- NUM_MENOR_ARREGLO.cpp
+++ NUM_MENOR_ARREGLO.cpp
@@ -1,26 +1,30 @@
 #include<stdio.h>
 #include<stdlib.h>
-int i,num[10];
+int i,num[10],help=0,h,num1;
 int j=60000,k=60000;
 int main()
 {
     printf("PROGRAMA QUE TE DA EL NUMERO MENOR Y EL QUE LE SIGUE\n\n");
     for (i=0;i<10;i++)
-    {
+     {
         printf("Dame el numero :%d ",i+1);
         scanf("%d", &num[i]);
-    if (num[i]<j)
-        {       
-        j = num[i];
-        } 
      }
-    for(i=0;i<10;i++)
-    {
-    if (num[i]<k && num[i] != j)
-    {
-                 k = num[i];
-    }
-    } printf("EL NUMERO MENOR ES :%d", j);
-      printf("\nEL NUMERO QUE LE SIGUE ES :%d",k);
+        printf("Cuantos numeros ?");
+       	scanf("%d", &num1);
+       	help =0;
+   	 for(h=0;h<num1;h++)
+     {
+     k=60000;
+     for(i=0;i<10;i++)
+     {
+            if (num[i]<k && num[i] >help)
+               {
+               k = num[i];
+               }
+    } printf("EL NUMERO MENOR ES :%d \n", k);
+      help = k;
+}
 system("PAUSE");
+return 0;
 }
